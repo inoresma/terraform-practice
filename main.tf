3f7fff445d038ff49499c568c668ca8a167e6bb5
inoresma <inoresma@gmail.com>
2025-08-19 22:16:08 -0400

    fix: a little mistake

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "nginx-server" {
 #ssh-keygen -t rsa -b 2048 -f "nginx-server-key"
 resource "aws_key_pair" "nginx_server_ssh" {
     key_name = "nginx-server-ssh"
-    public_key = file("nginx-server-key.pub")
+    public_key = file("nginx-server.key.pub")
 
     tags ={
       Name = "nginx-server-ssh"
